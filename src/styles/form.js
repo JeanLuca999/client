b8ddcb3c7e9e40c899a14968aeef7f9d65cfa79a
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-10-27 22:50:39 -0300

    styles(forms): set default forms styles

---
+++ src/styles/form.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Form = styled.form``;
+
+export const Field = styled.fieldset`
+  position: relative;
+  border: 1px solid #838383;
+  border-radius: 5px;
+  padding: 1rem;
+  margin-bottom: 2rem;
+`;
+
+export const Label = styled.label`
+  font-size: 12px;
+  color: #838383;
+  position: absolute;
+  top: -1rem;
+  left: 0;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  border: 0;
+  outline: 0;
+`;
