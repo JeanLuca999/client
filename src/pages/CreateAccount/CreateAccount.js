30c8d2d728aaae9c8e0fd57bb771c81c6cd1d43b
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-10-27 23:35:53 -0300

    feat(CreateAccount): create form

--- src/pages/CreateAccount/CreateAccount.js
+++ src/pages/CreateAccount/CreateAccount.js
@@ -2,34 +2,39 @@
 import { BannerForm } from "../../components/BannerForm";
 
 //STLYES
-import { Grid } from "../../styles/generics";
-import { Field, Form, Input, Label } from "../../styles/form";
+import { Box, Center, Grid } from "../../styles/generics";
+import { Anchor, Field, Form, Input, Label, Submit } from "../../styles/form";
 
 export const CreateAccount = () => {
   return (
     <Grid>
       <BannerForm />
-      <Form style={{ padding: "2rem" }}>
-        <Field>
-          <Label>Nome</Label>
-          <Input type="text" />
-        </Field>
-
-        <Field>
-          <Label>Email</Label>
-          <Input type="text" />
-        </Field>
-
-        <Field>
-          <Label>Senha</Label>
-          <Input type="password" />
-        </Field>
-
-        <Field>
-          <Label>Confirmar Senha</Label>
-          <Input type="password" />
-        </Field>
-      </Form>
+      <Center vertically>
+        <Box width="90%" maxWidth="500px" margin="0 auto">
+          <Form>
+            <Field>
+              <Label>Nome</Label>
+              <Input type="text" />
+            </Field>
+            <Field>
+              <Label>Email</Label>
+              <Input type="text" />
+            </Field>
+            <Field>
+              <Label>Senha</Label>
+              <Input type="password" />
+            </Field>
+            <Field>
+              <Label>Confirmar Senha</Label>
+              <Input type="password" />
+            </Field>
+            <Submit type="submit">Cadastrar-se</Submit>
+          </Form>
+          <Box margin="1rem 0">
+            <Anchor to="/login">Já tem uma conta? Entre aqui</Anchor>
+          </Box>
+        </Box>
+      </Center>
     </Grid>
   );
 };
