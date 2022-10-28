8abdd570fef928cbf5f3a7ac3bb5dc1822ec3f79
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-10-27 23:34:48 -0300

    style(form): create new styles

--- src/styles/form.js
+++ src/styles/form.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+import { Link } from "react-router-dom";
+
 export const Form = styled.form``;
 
 export const Field = styled.fieldset`
   position: relative;
   border: 1px solid #838383;
   border-radius: 5px;
-  margin-bottom: 2rem;
+  margin: 2rem 0;
 `;
 
 export const Label = styled.label`
@@ -23,4 +25,20 @@ export const Input = styled.input`
   outline: 0;
   padding: 1rem;
   border-radius: 5px;
+`;
+
+export const Submit = styled.button`
+  border: 0;
+  border-radius: 5px;
+  display: block;
+  width: 100%;
+  background-color: #838383;
+  color: #fff;
+  padding: 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+`;
+
+export const Anchor = styled(Link)`
+  color: #838383;
 `;
