01032b0dcb395a7c1a689c880cd7dc6fe89111c7
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-10-27 22:08:45 -0300

    feat(app): setup initial routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+//CONTEXT
+import { AuthProvider } from "./context/auth";
+
+//COMPONENTS
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>home page</h1>} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <h1>a protected page</h1>
+                </PrivateRoute>
+              }
+            />
+
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
 };
 
 export default App;
