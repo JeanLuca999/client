e5d8c72c417f197df125155cbe6fb41587d6782c
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-10-29 16:48:27 -0300

    feat(Login): create login page layout

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,97 @@
+//HOOKS
+import { useForm } from "../../hooks";
+import { useNavigate } from "react-router-dom";
+
+//COMPONENTS
+import { BannerForm } from "../../components/BannerForm";
+
+//STYLES
+import { Box, Center, Grid } from "../../styles/generics";
+import {
+  Anchor,
+  Field,
+  Form,
+  Input,
+  Label,
+  Submit,
+  Error,
+} from "../../styles/form";
+
+//HELPERS
+import { validateEmail, validatePassword } from "../../helpers";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const [fields, handleChange, onSubmit, validations] = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { email, password } = fields;
+
+  const handleValidateForm = () => {
+    return [
+      validations.register("email", validateEmail, "email inválido"),
+      validations.register("password", validatePassword, "senha inválida"),
+    ];
+  };
+
+  const handleFormFieldChange = (event) => {
+    const { name, value } = event.target;
+    handleChange(name, value);
+  };
+
+  const handleSubmit = (event) => {
+    event?.preventDefault();
+    console.log("post data");
+    navigate("/home");
+  };
+
+  return (
+    <Grid>
+      <BannerForm>
+        Entre para criar e visualizar <br /> postagens
+      </BannerForm>
+      <Center vertically>
+        <Box width="90%" maxWidth="500px" margin="0 auto">
+          <Form
+            method="post"
+            onSubmit={(event) =>
+              onSubmit(event, handleSubmit, handleValidateForm)
+            }
+          >
+            <Field>
+              <Label>Email</Label>
+              <Input
+                type="text"
+                name="email"
+                value={email}
+                onChange={handleFormFieldChange}
+              />
+            </Field>
+            {validations?.errors?.email && (
+              <Error>{validations?.errors?.email}</Error>
+            )}
+            <Field>
+              <Label>Senha</Label>
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleFormFieldChange}
+              />
+            </Field>
+            {validations?.errors?.password && (
+              <Error>{validations?.errors?.password}</Error>
+            )}
+
+            <Submit type="submit">Entrar</Submit>
+          </Form>
+          <Box margin="1rem 0">
+            <Anchor to="/register">Ainda não tem uma conta? Cadastre-se</Anchor>
+          </Box>
+        </Box>
+      </Center>
+    </Grid>
+  );
+};
