88fcd9fdb60f10c42bebfc28f9892b6769833030
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-11-02 12:53:23 -0300

    feat(services): create loginUser

--- src/services/index.js
+++ src/services/index.js
@@ -5,3 +5,8 @@ export const createUser = (data) => {
   const options = { headers: { "Content-Type": "application/json" } };
   return api.post(`/register`, data, options);
 };
+
+export const loginUser = (data) => {
+  const options = { headers: { "Content-Type": "application/json" } };
+  return api.post(`/login`, data, options);
+};
