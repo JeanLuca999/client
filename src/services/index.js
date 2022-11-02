bc4419c71517371b05a8a3a1726a8f456f4cab01
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-11-02 11:39:33 -0300

    refactor(services): refactor createUser

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,7 @@
 //API
 import { api } from "../api";
 
-export const createUser = async (data) => {
-  return api.post(`/register`, data);
+export const createUser = (data) => {
+  const options = { headers: { "Content-Type": "application/json" } };
+  return api.post(`/register`, data, options);
 };
