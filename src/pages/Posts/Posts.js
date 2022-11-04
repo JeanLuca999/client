186861890fa02fa19b5c40e1935b6413c5876add
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-11-03 21:28:07 -0300

    feat(pages): initial section in Posts page

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -1,3 +1,44 @@
+//COMPONENTS
+import { CreatePost } from "../../components/CreatePost/CreatePost";
+import { Box } from "../../styles/generics";
+
+//HOOKS
+import { useForm } from "../../hooks";
+import { validateNotEmpty } from "../../helpers";
+import { Error } from "../../styles/form";
+
 export const Posts = () => {
-  return <></>;
+  const [fields, handleChange, onSubmit, validations] = useForm({
+    content: "",
+  });
+  const { content } = fields;
+
+  const handlePostSubmit = () => {};
+
+  const handleValidateForm = () => {
+    return [
+      validations.register("content", validateNotEmpty, "campo obrigatório"),
+    ];
+  };
+
+  const handlePostContentChange = (content) => {
+    handleChange("content", content);
+  };
+
+  const handleSubmit = (event) => {
+    onSubmit(event, handlePostSubmit, handleValidateForm);
+  };
+
+  return (
+    <>
+      <Box margin="2rem 0">
+        <CreatePost
+          onChange={handlePostContentChange}
+          value={content}
+          handleSubmit={handleSubmit}
+          errors={validations?.errors?.content}
+        />
+      </Box>
+    </>
+  );
 };
