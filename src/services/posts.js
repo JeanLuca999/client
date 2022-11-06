c281d75b5b684c93c4cc7a6f26327830885b1fec
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-11-06 18:03:34 -0300

    feat(services): add new posts services

--- src/services/posts.js
+++ src/services/posts.js
@@ -9,3 +9,16 @@ export const createPost = (data) => {
   const options = { headers: { "Content-Type": "application/json" } };
   return api.post(`/posts`, data, options);
 };
+
+export const getPostsByUserId = (id) => {
+  return api.get(`/posts/user/${id}`);
+};
+
+export const updatePost = (id, data) => {
+  const options = { headers: { "Content-Type": "application/json" } };
+  return api.put(`/posts/${id}`, data, options);
+};
+
+export const deletePost = (id) => {
+  return api.delete(`/posts/${id}`);
+};
